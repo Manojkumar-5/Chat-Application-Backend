75007b7c0bf66be947fd2b7b03c74b527cc44205
Manojkumar-5 <66167395+Manojkumar-5@users.noreply.github.com>
2021-05-19 10:50:00 +0530

    Initial Commit

---
+++ src/winston.js
@@ -0,0 +1,53 @@
+var appRoot = require('app-root-path');
+
+const { createLogger, format, transports } = require('winston');
+const { combine, timestamp, splat,printf} = format;
+
+const myFormat = printf(({ level, message, timestamp }) => {
+  return `${timestamp} ${level}`;
+});
+
+
+var options = {
+  file: {
+    level: 'info',
+    filename: `${appRoot}/logs/app.log`,
+    handleExceptions: true,
+    json: true,
+    maxsize: 5242880, // 5MB
+    maxFiles: 5,
+    colorize: false,
+  },
+  console: {
+    level: 'debug',
+    handleExceptions: true,
+    json: false,
+    colorize: true,
+  },
+};
+
+// instantiate a new Winston Logger with the settings defined above
+var logger = createLogger({
+
+  format: combine(
+    timestamp(),
+    splat(),
+    myFormat
+  ),
+
+  transports: [
+    new transports.File(options.file),
+    new transports.Console(options.console)
+  ],
+  exitOnError: false, // do not exit on handled exceptions
+});
+
+logger.stream = {
+  write: function(message, encoding) {
+    // use the 'info' log level so the output will be picked up by both transports (file and console)
+    logger.info(message);
+  
+  },
+};
+
+module.exports = logger;
